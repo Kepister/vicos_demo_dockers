82b45cc6cea9e6f1e92316aac3cce6f8667ba8d2
DemoOS <peter.mlakar3@gmail.com>
2021-11-18 11:36:50 +0100

    Initial commit.

---
+++ poly_docker/scripts/echolib_wrapper.py
@@ -0,0 +1,111 @@
+import time
+import cv2
+
+from echolib import pyecho
+import echocv
+
+from threading import Thread, Lock
+
+
+class EcholibWrapper:
+    def __init__(self, detection_method, out_channel, in_channel):
+
+        self.loop   = pyecho.IOLoop()
+        self.client = pyecho.Client()
+        self.loop.add_handler(self.client)
+
+        self.enabled = False
+
+        self.dockerCommandOut = pyecho.Publisher(self.client, out_channel, "numpy.ndarray")
+        self.dockerReady      = pyecho.Publisher(self.client, "containerReady", "int")
+
+        self.dockerCommandIn  = pyecho.Subscriber(self.client, in_channel, "int", self.callback)
+        self.dockerCameraIn   = pyecho.Subscriber(self.client, "cameraStream", "numpy.ndarray", self.cameraCallback)
+
+        self.detection_method = detection_method
+
+        self.frameInLock   = Lock()
+        self.frameOutLock  = Lock()
+
+        self.frameIn    = None
+        self.frameInNew = False
+
+        self.frameOut    = None
+        self.frameOutNew = False 
+
+        self.closing = False
+
+    def callback(self, message):
+
+        # TODO Maybe add some threading proctection?
+        self.enabled = True if (pyecho.MessageReader(message).readInt() != 0) else False
+
+        print("Got command {}".format(self.enabled))
+        
+
+    def cameraCallback(self, message):
+
+        self.frameInLock.acquire()
+        self.frameIn    = echocv.readMat(pyecho.MessageReader(message))
+        self.frameInNew = True
+        self.frameInLock.release()
+
+    def process(self):
+        
+        # TODO Using self.closing in a thread unsafe manner
+        while not self.closing:
+
+            frame = None
+
+            if self.enabled and self.frameInNew:
+
+                print("Detecting polyps...")
+
+                self.frameInLock.acquire()
+                frame = self.frameIn.copy()
+                self.frameInNew = False
+                self.frameInLock.release()
+            
+                frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+                frame = self.detection_method.predict(frame)
+                self.enabled = False
+
+            if frame is not None:
+                self.frameOutLock.acquire()
+                self.frameOut    = frame
+                self.frameOutNew = True
+                self.frameOutLock.release()
+
+    def run(self, wait_sec=10, sleep_sec=0):
+
+        self.loop.wait(10) # Maybe registering subscribers and publishers?
+
+        writer = pyecho.MessageWriter()
+        writer.writeInt(1)
+        self.dockerReady.send(writer)
+
+        thread = Thread(target = self.process)
+        thread.start()
+
+        print("Starting...")
+
+        while self.loop.wait(wait_sec):
+
+            self.frameOutLock.acquire()
+            if self.frameOutNew:
+                writer = pyecho.MessageWriter()
+                echocv.writeMat(writer, self.frameOut)
+                self.dockerCommandOut.send(writer)
+
+                self.frameOutNew = False
+            self.frameOutLock.release()
+
+            if sleep_sec > 0:
+                time.sleep(sleep_sec)
+
+        print("Stop")
+
+        thread.join()
+
+
+
---
+++ traffic_docker/build-dockers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker build detectron -t detectron-traffic-signs:ubuntu16.04-cuda10.0-cudnn7  \
+                        --build-arg CUDA_VERSION=11.1.1-cudnn8 \
+                        --build-arg OS_VERSION=ubuntu16.04
+
+docker build vicos-demo -t tsr-vicos-demo:ubuntu16.04-cuda10.0-cudnn7-resnet50 \
+                        --build-arg OS_VERSION=ubuntu:16.04 \
+                        --build-arg DETECTRON_IMAGE_RUNTIME=detectron-traffic-signs:ubuntu16.04-cuda10.0-cudnn7 \
+                        --build-arg BACKBONE=resnet50
+
+docker build vicos-demo -t tsr-vicos-demo:ubuntu16.04-cuda10.0-cudnn7-resnet101 \
+                        --build-arg OS_VERSION=ubuntu:16.04 \
+                        --build-arg DETECTRON_IMAGE_RUNTIME=detectron-traffic-signs:ubuntu16.04-cuda10.0-cudnn7 \
+                        --build-arg BACKBONE=resnet101
